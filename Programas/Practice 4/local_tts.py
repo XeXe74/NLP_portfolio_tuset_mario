8c6da0cacbee91e52ef1ab62fb4e0328e10b838b
XeXe74 <mariotg1000@gmail.com>
2026-04-20 11:54:39 +0200

    Added a function that allows the user to select the voice in the local text to speech

--- Programas/Practice 4/local_tts.py
+++ Programas/Practice 4/local_tts.py
@@ -21,8 +21,27 @@ def speak_local(text: str, voice_index: int = 0) -> None:
     engine.say(text)
     engine.runAndWait()  # Block until speaking is finished
 
+def list_voices():
+    """
+    Prints all available voices on the system.
+    """
+    voices = engine.getProperty("voices") # Get available voices
+
+    # Print only the index because the list is huge
+    print(f"Available voices: 0 to {len(voices) - 1}")
+    return len(voices)
+
 # Test
 if __name__ == "__main__":
     configure_engine()
+    num_voices = list_voices() # Get the voices count
+
+    # Allow user to select a voice index
+    voice_index = int(input("Select voice index: "))
+    # Validate the voice index
+    if not 0 <= voice_index <= num_voices - 1:
+        print("Invalid index, using default (0).")
+        voice_index = 0
+
     text = input("Enter text to speak: ")
-    speak_local(text)
+    speak_local(text, voice_index=voice_index)
